ddbb48a1e809bf23b6759070d83ff886c332626b
Johnny Gérard <john@jgerard.dev>
2026-05-31 20:16:14 +0200

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+const NotFoundPage: FC = () => (
+  <main>
+    <h1 className="mt-20 text-center text-4xl font-bold">Not Found</h1>
+  </main>
+);
+
+export default NotFoundPage;
