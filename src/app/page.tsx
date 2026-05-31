32759522eab164d242448a923d6c150ffc5388a0
Johnny Gérard <john@jgerard.dev>
2026-05-31 19:25:40 +0200

    feat: improve homepage design, content, and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
-}
+import type { FC } from "react";
+
+const HomePage: FC = () => (
+  <main>
+    <h1 className="mt-20 text-center text-4xl font-bold">Homepage</h1>
+  </main>
+);
+
+export default HomePage;
